e596603b3b66282c88bd44163115dcd06db4be78
virux <jantonioascencio10@gmail10.com>
2026-09-11 19:20:27 -0300

    primer test ok

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -3,4 +3,12 @@ const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
 
+    test("GET /cafes devuelve 200 y un arreglo con al menos un café", async () => {
+        const response = await request(server).get("/cafes");
+
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toBeGreaterThan(0);
+    });
+
 });
